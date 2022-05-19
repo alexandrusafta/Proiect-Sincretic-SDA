1c9e03c0105d8a766f4f7a251491d8b85bb890fc
alexandrusafta <100847947+alexandrusafta@users.noreply.github.com>
2022-05-19 13:16:18 +0300

    Update Pronuntabilitate.cpp

--- Proiect Sincretic Safta Alexandru/Pronuntabilitate.cpp
+++ Proiect Sincretic Safta Alexandru/Pronuntabilitate.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 int main()
 {
+	
+	//Se dă fișier text. Se cere să se calculeze pronunțabilitatea P a textului din fișier, după formula:
+	//P = (număr total de vocale / număr total de consoane) * (1 / lungimea medie a cuvintelor).
+	//Cuvintele se consideră separate prin spații sau semne de punctuație (virgula, punct, punct și
+	//virgulă, semnul întrebării, semnul exclamării).
+	
+	
 	ifstream f("input.txt");
 
 	//Declarare variabile
